fc101c767c4e846183c4653449818f668e9065be
samil96 <1628112@utp.edu.pe>
2020-08-22 15:38:54 -0500

    separación de widgets

--- primeros_pasos/lib/main.dart
+++ primeros_pasos/lib/main.dart
@@ -6,36 +6,103 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        backgroundColor: Colors.pink[100],
-        appBar: AppBar(
-          title: Text('Welcome to Flutter',
-          style: TextStyle(
-            color: Colors.blue,
-          )
-          ),
-          backgroundColor: Colors.pink,
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            FlutterLogo(
-              size: 200,
-              colors: Colors.pink,
-            ),
-             Text(
-               'Hello World',
-              style: TextStyle(
-                fontSize: 40,
-                color: Colors.blue,
-              ),
-              textAlign: TextAlign.center,
-             ),           
-          ],
-        ),
-      ),
+      title: 'Login',
+      home: Home(),
     );
   }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: fondo(),
+    );
+  }
+}
+
+//Separar widgets
+//funcion que devuelve un widget
+
+Widget fondo() {
+  return Container(
+    decoration: BoxDecoration(
+        image: DecorationImage(
+            image: NetworkImage(
+                "https://i.pinimg.com/originals/6b/a0/a5/6ba0a5b7117010dd2972ab157d26d28d.jpg"),
+            fit: BoxFit.cover)),
+    child: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          SizedBox(height: 120,),
+          titulo(),
+          campoUsuario(),
+          campoContrasenia(),
+          SizedBox(height: 5,),
+          botonEnter()
+        ],
+      ),
+    ),
+  );
+}
+
+Widget titulo() {
+  return Text(
+    "Iniciar Sesión",
+    style: TextStyle(
+      color: Colors.black87,
+      fontSize: 35.0,
+      fontWeight: FontWeight.bold,
+    ),
+  );
+}
+
+Widget campoUsuario() {
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+    child: TextField(
+      decoration: InputDecoration(
+        hintText: "Usuario",
+        fillColor: Colors.white,
+        filled: true,
+      ),
+    ),
+  );
+}
+
+Widget campoContrasenia() {
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+    child: TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: "Contraseña",
+        fillColor: Colors.white,
+        filled: true,
+      ),
+    ),
+  );
+}
+
+Widget botonEnter() {
+  return FlatButton(
+    color: Colors.pink[200],
+    padding: EdgeInsets.symmetric(horizontal: 135.0, vertical: 10),
+    onPressed: (){
+      //codigo
+    },
+    child: Text(
+      "Ingresar",
+      style: TextStyle(
+        color: Colors.black,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+  );
 }
